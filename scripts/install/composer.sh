85aae0d7715584cd32ff92351affb06aa952be55
Russell Jones <jonesrussell42@gmail.com>
2019-11-24 17:53:35 -0500

    fix path

--- scripts/install/composer.sh
+++ scripts/install/composer.sh
@@ -2,7 +2,7 @@
 
 # From https://getcomposer.org/download/
 php -r "copy('https://getcomposer.org/installer', '/tmp/composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php --install-dir="${LOCAL_BIN}" --filename=composer
+php -r "if (hash_file('sha384', '/tmp/composer-setup.php') === 'a5c698ffe4b8e849a443b120cd5ba38043260d5c4023dbf93e1558871f1f07f58274fc6f4c93bcfd858c6bd0775cd8d1') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('/tmp/composer-setup.php'); } echo PHP_EOL;"
+php /tmp/composer-setup.php --install-dir="${LOCAL_BIN}" --filename=composer
 php -r "unlink('/tmp/composer-setup.php');"
 
