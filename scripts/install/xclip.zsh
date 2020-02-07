a8c3671b28596dd111ecb29956f68d73e8823e69
Russell Jones <jonesrussell42@gmail.com>
2020-02-07 05:54:48 -0500

    Clean up install scripts

--- scripts/install/xclip.zsh
+++ scripts/install/xclip.zsh
@@ -1,6 +1,12 @@
 #!/bin/zsh
 
-# Install and setup xclip
+# Install xclip
+sudo apt install xclip
+
+# Send xclip's selection to clipboard by default
+echo 'alias xclip="xclip -selection clipboard"' >> ${HOME}/.zshrc
+
+source ${HOME}/.zshrc
 
 # du -hd 1 -t 100M
 
