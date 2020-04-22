4d61600ec551859735eacaf7dbd3ed0fa8649835
Russell Jones <jonesrussell42@gmail.com>
2020-04-21 20:58:44 -0400

    fixed comments

--- scripts/drupal-create-project.sh
+++ scripts/drupal-create-project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-// @TODO check for existance of composer
+# @TODO check for existance of composer
 COMPOSER=$(which composer)
-// @TODO check for arguments
+# @TODO check for arguments
 $COMPOSER create-project drupal-composer/drupal-project:8.x-dev $@ --no-interaction
+
