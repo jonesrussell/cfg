09b84f2c7d37c23092990150b0ad5cb37a983aea
Russell Jones <jonesrussell42@gmail.com>
2020-05-13 23:38:35 -0400

    WIP

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const shell = require('shelljs');
 
 // Make sure git is installed
+shell.echo('git? ');
+
 if (!shell.which('git')) {
-  shell.echo('Error: This script requires git');
-  shell.exit(1);
+    shell.echo('🖕');
+    shell.exit(1);
 }
+
+shell.echo('🤘');
+shell.echo("\n\n");
 
 // Run external script synchronously
 if (shell.exec('./install.sh').code !== 0) {
   shell.echo('Error: Setup failed');
   shell.exit(1);
 }
+
